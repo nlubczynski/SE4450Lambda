fe256184f360ddc9913a6d971173cbb1d4642bc9
blunn2 <braden.lunn@gmail.com>
2015-03-31 20:43:45 -0400

    brought in MRunit to test map job

---
+++ hadoop/src/test/java/com/se4450/hadoop/test/WriteToHbaseTest.java
@@ -0,0 +1,33 @@
+package com.se4450.hadoop.test;
+
+import java.io.IOException;
+
+import org.apache.hadoop.hbase.client.Mutation;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.se4450.hadoop.WriteToHbase.Map;
+
+public class WriteToHbaseTest {
+	MapDriver<LongWritable, Text, Text, Text> mapDriver;
+	ReduceDriver<Text, Text, ImmutableBytesWritable, Mutation> reduceDriver;
+
+	@Before
+	public void setUp() {
+		Map mapper = new Map();
+		mapDriver = MapDriver.newMapDriver(mapper);
+	}
+
+	@Test
+	public void testMapper() throws IOException {
+		mapDriver.withInput(new LongWritable(),
+				new Text("1-12345679890123 100"));
+		mapDriver.withOutput(new Text("1-12345679890123"), new Text("100"));
+		mapDriver.runTest();
+	}
+}
